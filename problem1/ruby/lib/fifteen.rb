27b02a1ae28418ace1b9a718d3e8fac9b28b5ff4
David Gustafson <git@radicalskeptic.net>
2013-07-22 20:01:51 -0700

    completed problem one for ruby

--- problem1/ruby/lib/fifteen.rb
+++ problem1/ruby/lib/fifteen.rb
@@ -2,7 +2,16 @@
 
 
 def total upper_boundary
-
+  sum = 0
+  upper = upper_boundary
+  counter = 0;
+  while counter < upper
+    if( counter % 15 == 0 || counter % 5 == 0 || counter % 3 == 0)
+      sum += counter
+    end
+    counter += 1
+  end
+  sum
 end
 
 
