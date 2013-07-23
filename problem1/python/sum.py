4c8089ea14ae74dc5518dcdf695d4a8d8089a457
David Gustafson <git@radicalskeptic.net>
2013-07-22 23:53:35 -0700

    added solution in python

---
+++ problem1/python/sum.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+
+def total(upperBoundary):
+    count = 0
+    upper = upperBoundary
+    sum = 0
+
+    while count < upper:
+        if count % 15 == 0:
+            sum += count
+        elif (count % 3 == 0 or count % 5 == 0):
+            sum += count
+        count += 1
+    return sum
+
+class TotalTest(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(total(10), 23)
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
