136a504bf66b5858472574bcfe913f466b67c155
David Gustafson <git@radicalskeptic.net>
2013-07-22 20:38:24 -0700

    got jasmin working and solved the euler problem in js

---
+++ problem1/javascript/spec/TotalSpec.js
@@ -0,0 +1,12 @@
+/*global describe */
+/*global it */
+/*global expect */
+/*global total */
+
+describe("Total", function() {
+
+  it("it should return 23 when given 10", function() {
+    expect(window.total(10)).toEqual(23);
+  });
+
+});
---
+++ problem1/javascript/src/Total.js
@@ -0,0 +1,16 @@
+
+
+window.total = function(upperBoundary){
+  var count = 0;
+  var upper = upperBoundary;
+  var sum = 0;
+
+  for(count; count < upper; count += 1){
+    if (count % 15 === 0){
+      sum += count;
+    } else if (count % 3 === 0 || count % 5 === 0){
+      sum+= count;
+    }
+  }
+  return sum;
+};
